3cf42a6d9163e75f9f5d8f5dc606784751b45d34
Adityaptil51 <adityaapatil51@gmail.com>
2025-10-18 11:30:26 +0530

    save

--- CIE-1.py
+++ CIE-1.py
@@ -1,12 +1,8 @@
 a = input("Enter a name: ")
-rev = a[:: -1]
-print("The reversed number is: {rev}")
+rev = a[::-1]
+print(f"The reversed name is: {rev}")
 
 if a == rev:
     print("The name is a palindrome")
 else:
     print("The name is not a palindrome")
-
-
-
-
